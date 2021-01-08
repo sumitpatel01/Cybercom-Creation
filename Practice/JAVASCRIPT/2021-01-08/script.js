a3f47dc2d106d67136fc292b815cee5c1f45f030
sumit <sapoliyasumit5674@gmail.com>
2021-01-08 21:16:49 +0530

    added today's practice

---
+++ Practice/JAVASCRIPT/2021-01-08/script.js
@@ -0,0 +1,41 @@
+//console.log("hello world!!!");
+
+/* variable and datatypes */
+var firstName = 'john';
+console.log(firstName);
+
+var lastName = 'smith';
+var age = 38;
+
+//number,string,boolean,undefined,null are primitive datatypes of js
+var fullAge = true;
+console.log(fullAge);
+
+var job;  // undefined
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+/*
+
+this
+is 
+a
+multiline
+comments
+
+*/
+
+
+/* variable mutations and type coercion */
+console.log(firstName + ' ' + age);
+
+var isMarried = false;
+console.log(isMarried);
+
+age = "thirty eight";
+
+alert(hello);
+
+var lastname = prompt("what is his lastname");
